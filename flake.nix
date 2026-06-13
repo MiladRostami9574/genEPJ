2bd1e75d5c3befa5892d972b0e83639b832afa4a
MiladRostami9574 <108143738+MiladRostami9574@users.noreply.github.com>
2026-06-13 14:59:25 -0400

    Add files via upload

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Development shell for genEPJ";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.python311
+            pkgs.python311Packages.pip
+            pkgs.python311Packages.virtualenv
+            pkgs.git
+          ];
+        };
+      });
+    };
+}
